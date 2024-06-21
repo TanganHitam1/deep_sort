3b5eed749e94eeedc8132d3abac9481afe75aabf
TanganHitam1 <blackjackm2709@gmail.com>
2024-06-21 08:07:41 +0700

    chore: Update tf_slim import in freeze_model.py

--- tools/freeze_model.py
+++ tools/freeze_model.py
@@ -1,8 +1,10 @@
 # vim: expandtab:ts=4:sw=4
 import argparse
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
+from tensorflow.python.framework import ops
+# import tensorflow.contrib.slim as slim
 
+import tf_slim as slim
 
 def _batch_norm_fn(x, scope=None):
     if scope is None:
